c0a41e66f7d3e4b7256d9f5a62c8fed85f798336
tcnksm <nsd22843@gmail.com>
2013-08-20 22:24:17 +0900

    Add class method for easy use

--- lib/random_japanese_string.rb
+++ lib/random_japanese_string.rb
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
-require "random_japanese/version"
+require "random_japanese_string/version"
 require "yaml"
 
-class RandomJapanese
-  HIRA = YAML.load_file(File.expand_path(File.join('..', 'data', 'hiragana.yml'), __FILE__))
-  KATA = YAML.load_file(File.expand_path(File.join('..', 'data', 'katakana.yml'), __FILE__))
-  KANJI = YAML.load_file(File.expand_path(File.join('..', 'data', 'kanji.daily.yml'), __FILE__))
+class RandomJapaneseString
+  HIRA   = YAML.load_file(File.expand_path(File.join('..', 'data', 'hiragana.yml'), __FILE__))
+  KATA   = YAML.load_file(File.expand_path(File.join('..', 'data', 'katakana.yml'), __FILE__))
+  KANJI  = YAML.load_file(File.expand_path(File.join('..', 'data', 'kanji.daily.yml'), __FILE__))
+  MIX    = HIRA*15 + KATA*15 + KANJI
   
+  def self.hiragana size
+    HIRA.sample(size).join
+  end
+
+  def self.katakana size
+    KATA.sample(size).join
+  end
+
+  def self.kanji size
+    KANJI.sample(size).join
+  end
+
+  def self.generate size
+    MIX.sample(size).join
+  end
+
   def hiragana size
     HIRA.sample(size).join
   end
@@ -19,7 +36,7 @@ class RandomJapanese
     KANJI.sample(size).join
   end
 
-  def all size
-    (HIRA+KATA+KANJI).sample(size).join
+  def generate size
+    MIX.sample(size).join
   end
 end
--- spec/random_japanese_string_spec.rb
+++ spec/random_japanese_string_spec.rb
@@ -2,32 +2,61 @@
 
 require "spec_helper"
 
-describe RandomJapanese do
-  before do
-    @rj = RandomJapanese.new
+describe RandomJapaneseString do
+
+  describe "class method" do
+    describe ".hiragana()" do
+      subject { RandomJapaneseString.hiragana(8) }
+      its(:length) { should eq 8 }
+      it {should match(/^[\p{hiragana}]{8}$/)}
+    end
+    
+    describe ".katakana()" do
+      subject { RandomJapaneseString.katakana(10) }
+      its(:length) { should eq 10}
+      it {should match(/^[\p{katakana}]{10}$/)}
+    end
+
+    describe ".kanji()" do
+      subject { RandomJapaneseString.kanji(5) }
+      its(:length) { should eq 5}
+      it {should match(/^[#{Moji.zen}]{5}$/)}
+    end
+    
+    describe ".generate()" do
+      subject { RandomJapaneseString.generate(10) }
+      its(:length) { should eq 10}
+      it {should match(/^[#{Moji.zen}]{10}$/)}
+    end
+
   end
 
-  describe "#hiragana()" do
-    subject { @rj.hiragana(8) }
-    its(:length) { should eq 8 }
-    it {should match(/^[\p{hiragana}]{8}$/)}
-  end
+  describe "instance method" do
+    
+    before {@rj = RandomJapaneseString.new }
+    describe "#hiragana()" do
+      subject { @rj.hiragana(30) }
+      its(:length) { should eq 30 }
+      it {should match(/^[\p{hiragana}]{30}$/)}
+    end
 
-  describe "#katakana()" do
-    subject { @rj.katakana(10) }
-    its(:length) { should eq 10}
-    it {should match(/^[\p{katakana}]{10}$/)}
-  end
+    describe "#katakana()" do
+      subject { @rj.katakana(30) }
+      its(:length) { should eq 30}
+      it {should match(/^[\p{katakana}]{30}$/)}
+    end
 
-  describe "#kanji()" do
-    subject { @rj.kanji(5) }
-    its(:length) { should eq 5}
-    it {should match(/^[#{Moji.zen}]{5}$/)}
-  end
+    describe "#kanji()" do
+      subject { @rj.kanji(30) }
+      its(:length) { should eq 30}
+      it {should match(/^[#{Moji.zen}]{30}$/)}
+    end
 
-  describe "#all()" do
-    subject { @rj.all(10) }
-    its(:length) { should eq 10}
-    it {should match(/^[#{Moji.zen}]{10}$/)}
+    describe "#generate()" do
+      subject { @rj.generate(30) }
+      its(:length) { should eq 30}
+      it {should match(/^[#{Moji.zen}]{30}$/)}
+    end
   end
+  
 end
